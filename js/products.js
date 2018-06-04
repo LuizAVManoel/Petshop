9f8503148219d8c978a5fd50700bc54d53a21e93
Guilherme Vicentin <guilherme.vicentin@usp.br>
2018-06-04 18:21:45 -0300

    lot of things

--- js/products.js
+++ js/products.js
@@ -1,22 +1,55 @@
-function fetchProductsData(productsFile, filter = []) {
+function fetchProduct(productsFile, id) {
+    return new Promise(resolve => {
+        $.getJSON('data/' + productsFile + '.json', function(data) {
+            var product = undefined;
+
+            for (var i = 0; i < data.length; i++) {
+                var element = data[i];
+
+                if (element['id'] === id) {
+                    product = element;
+                    break;
+                }
+            }
+
+            resolve(product);
+        });
+    });
+}
+
+function fetchAllProducts(productsFile, categorie = '') {
     var result = [];
     
     return new Promise(resolve => {
-        $.getJSON(productsFile, function(data) {
-            data.forEach(function(element) {
-                var finalProduct = {
-                    'categoria': element.categoria,
-                    'produtos': []
-                };
-            
-                if (filter.length == 0 || filter.indexOf(element.categoria) > -1) {
-                    element.produtos.forEach(function(product) {
-                        finalProduct['produtos'].push(product);
-                    });
-                    result.push(finalProduct);
+        $.getJSON('data/' + productsFile + '.json', function(data) {
+            data.forEach(function(element) {           
+                if (categorie === '' || element['categoria'] === categorie) {
+                    result.push(element);
                 }
             });
             resolve(result);
         });
     });
+}
+
+function createProductPreview(product) {
+    return `
+        <div class="col-md-3 arrival-grid simpleCart_shelfItem">
+            <div class="grid-arr">
+                <div class="grid-arrival">
+                    <figure>		
+                        <a href="#product?id=` + product['id'] + `">
+                            <div class="grid-img">
+                                <img src="` + product['foto'] + `" class="img-responsive" alt="">
+                            </div>		
+                        </a>		
+                    </figure>	
+                </div>
+                <div class="women" style="min-height: 100px;">
+                    <h6><a href="#product?id=` + product['id'] + `">` + product['nome'] + `</a></h6>
+                    <p><em class="item_price">R$ ` + product['preco'].toFixed(2) + `</em></p>
+                    <a href="#product?id=` + product['id'] + `"data-text="Comprar" class="my-cart-b item_add">Comprar</a>
+                </div>
+            </div>
+        </div>`;
 }
--- js/user.js
+++ js/user.js
@@ -1,8 +1,125 @@
-var user = function() {
-    this.isLogged = false;
+/** Variáveis globais para controlar o usuário */
+var user = {
+    'logged': false,
+    'id': 0,
+    'num_itens': 0,
+    'total_cart': 0,
+    'services': [],
+    'products': []
+};
+
+var userDB = [
+    {
+        'name': 'Admin',
+        'email': 'petshop@petshop.com',
+        'password': 'admin'
+    }
+];
+
+var updateNavCallback = undefined;
+var updateFooterCallback = undefined;
+
+/**  */
+function updateStaticContent() {
+    updateNavCallback();
+    updateFooterCallback();
 }
 
-function hasLoggedUser() {
-    return gUser.isLogged;
+function userLogin(email, password) {
+    for (var i = 0; i < userDB.length; i++) {
+        var element = userDB[i];
+        if(element['email'] == email && element['password'] == password) {
+            user['logged'] = true;
+            user['id'] = i;
+            resetUserData();
+            updateStaticContent();
+            return true;
+        }
+    }
+    return false;
 }
 
+function userLogout() {
+    user['logged'] = false;
+    updateStaticContent();
+}
+
+function resetUserData() {
+    user['num_itens'] = 0;
+    user['total_cart'] = 0;
+    user['products'] =  [];
+    updateStaticContent();
+}
+
+function isUserLogged() {
+    return user['logged'];
+}
+
+function getUserInfo() {
+    return userDB[user['id']];
+}
+
+function addUser(name, email, password) {
+    var newUser = {
+        'name': name,
+        'email': email,
+        'password': password
+    }
+    userDB.push(newUser);
+}
+
+function userAddService(date, type, hour) {
+    if (isUserLogged()) {
+        var newService = {
+            'date': date,
+            'type': type,
+            'hour': hour
+        };
+        user['services'].push(newService);
+    }
+}
+
+function userGetServices() {
+    return user['services'];
+}
+
+function userAddProduct(product, count=1) {
+    if (isUserLogged()) {
+        user['num_itens'] += count;
+        user['total_cart'] += count * product['preco'];
+        updateNavCallback();
+
+        for(var i = 0; i < user['products'].length; i++) {
+            var existProduct = user['products'][i];
+            if (product['id'] == existProduct['id']) {
+                existProduct['count'] += count;
+                
+                return;
+            }
+        }
+
+        var newProduct = {
+            'id': product['id'],
+            'count': count
+        };
+        user['products'].push(newProduct);
+    }
+}
+
+function userRemoveProduct(productToRemove) {
+    var index = -1;
+
+    for (var i = 0; i < user['products'].length; i++) {
+        var prod = user['products'];
+        if (productToRemove['id'] === prod['id']) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index > -1) {
+        user['num_itens'] -= prod['count'];
+        user['total_cart'] -= prod['count'] * productToRemove['preco'];
+        user['products'].splice(index, 1);
+    }
+}
